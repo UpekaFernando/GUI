939e00cb47ec9edd6c8055f56cdd31b1185518cb
upeka fernando <upekakaushal@gmail.com>
2025-01-06 00:52:06 +0530

    complteted dashboard

--- desktop application/shopmanegement/UserDashboard.xaml.cs
+++ desktop application/shopmanegement/UserDashboard.xaml.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
-using System.Windows;
 
 namespace ShopManagement
 {
@@ -21,6 +8,26 @@ namespace ShopManagement
         public UserDashboard()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == "Search for a shop or product...")
+            {
+                textBox.Text = string.Empty;
+                textBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "Search for a shop or product...";
+                textBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
         }
     }
 }
